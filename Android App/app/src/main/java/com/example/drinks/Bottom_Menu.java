1df73600678e3c35071acee0e025065dfb6f3d5b
cedriklesage <93670007+cedriklesage@users.noreply.github.com>
2023-02-03 12:21:02 -0500

    Correction thème

--- Android App/app/src/main/java/com/example/drinks/Bottom_Menu.java
+++ Android App/app/src/main/java/com/example/drinks/Bottom_Menu.java
@@ -30,8 +30,7 @@ public class Bottom_Menu extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_search, R.id.navigation_compte, R.id.navigation_favorites)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_bottom_menu);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+
     }
 
 }
